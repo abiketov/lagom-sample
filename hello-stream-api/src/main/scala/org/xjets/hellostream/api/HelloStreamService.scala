c304dddeb9eca898139c6deb04ecbbc02bf5cb37
abiketov <abiketov@hotmail.com>
2019-02-10 18:36:43 -0500

    First commit

---
+++ hello-stream-api/src/main/scala/org/xjets/hellostream/api/HelloStreamService.scala
@@ -0,0 +1,21 @@
+package org.xjets.hellostream.api;
+
+/**
+  * The Hello stream interface.
+  *
+  * This describes everything that Lagom needs to know about how to serve and
+  * consume the HelloStream service.
+  */
+trait HelloStreamService extends Service {
+
+  def stream: ServiceCall[Source[String, NotUsed], Source[String, NotUsed]]
+
+  override final def descriptor = {
+    import Service._
+
+    named("hello-stream")
+      .withCalls(
+        namedCall("stream", stream)
+      ).withAutoAcl(true)
+  }
+}
